adf0168fbe65856c1b662a502ac8d9f49ba3c034
ayaooooooooooo <59346123+ayaooooooooooo@users.noreply.github.com>
2020-01-30 15:00:27 -0800

    Finished all Unit 00 assignemnts

--- Unit00/src/AsciiBox.java
+++ Unit00/src/AsciiBox.java
@@ -10,8 +10,17 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("Anthony Yao \t  1/30/20 \n\n" );
 		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA" );
+		System.out.println("///////////////////////// " );
+		System.out.println("///////////////////////// " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ " );
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ " );
+		
 
 	}
 }
--- Unit00/src/Variables.java
+++ Unit00/src/Variables.java
@@ -18,16 +18,22 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
-
+		short shortOne = 32767;
+		int intOne = -2000000000;
+		long longOne = 1231231231;
 
 		//decimal variables
-
+		float floatOne = 120.7f;
+		double doubleOne = 4.234;
+		
 
 		//other integer types
-
+		char charOne = 'A';
 
 		//other types
-
+		boolean boolOne = false;
+		String stringOne = "Hello World";
+		
 
 		//output your information here
 		System.out.println("/////////////////////////////////");
@@ -35,8 +41,24 @@ public class Variables
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
-
-
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*           real types          *");
+		System.out.println("*                               *");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*           other types         *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+boolOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t*");
+		System.out.println("/////////////////////////////////");
 
 
 
